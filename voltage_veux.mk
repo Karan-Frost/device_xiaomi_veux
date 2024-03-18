a09ede48b1bfed81b9fffb95fc9de37d51193948
Frost <karanraj132804@gmail.com>
2024-03-18 18:28:58 +0530

    veux: Welcome to VoltageOS

--- voltage_veux.mk
+++ voltage_veux.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023-2024 The LineageOS Project
+# Copyright (C) 2023-2024 voltageOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,21 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
+# Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_AOSP_RECOVERY := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2201116SG
-PRODUCT_NAME := aosp_veux
+PRODUCT_NAME := voltage_veux
 PRODUCT_SYSTEM_NAME := veux_global
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Inherit some common device props
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="veux_global-user 13 TKQ1.221114.001 V14.0.6.0.TKCMIXM release-keys" \
